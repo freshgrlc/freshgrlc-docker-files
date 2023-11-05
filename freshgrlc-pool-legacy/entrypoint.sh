45ee77db8fb5cebd043e37a23a9159b562e801eb
Erik van `t Wout <erik@ewnetworks.eu>
2023-11-06 00:42:41 +0100

    freshgrlc-pool-legacy: Fix docker image build

--- freshgrlc-pool-legacy/entrypoint.sh
+++ freshgrlc-pool-legacy/entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-mv pool.js pool.js.orig
-sed 's/127.0.0.1/'"${DAEMON_HOST}"'/g' < pool.js.orig > pool.js
+sed -E -i.old 's/^ {12}"host":.*$/"host": "'"${DAEMON_HOST}"'",/' pool.js
+sed -E -i.old 's/^ {12}"port":.*$/"port": '"${DAEMON_PORT}"',/' pool.js
+sed -E -i.old 's/^ {12}"user":.*$/"user": "'"${DAEMON_USERNAME}"'",/' pool.js
+sed -E -i.old 's/^ {12}"password":.*$/"password": "'"${DAEMON_PASSWORD}"'",/' pool.js
 
-cat > credentials.js <<EOF
-module.exports.walletApiToken = '${WALLET_TOKEN}';
-EOF
-
-redis-server > /var/log/redis.log 2>&1 &
+sed -E -i.old 's/^ {8}"host":.*$/"host": "'"${P2P_HOST}"'",/' pool.js
+sed -E -i.old 's/^ {8}"port":.*$/"port": '"${P2P_PORT}"',/' pool.js
 
 if [ "${1:0:1}" = '-' ]; then
     set -- "node" "pool.js" "$@"
